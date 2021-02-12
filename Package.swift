fd0416c6d95bdf5fd805515f04f4bcdff420a325
Enchan <enchant0130@gmail.com>
2021-02-13 00:09:30 +0900

    [Modify] dependencies

--- Package.swift
+++ Package.swift
@@ -8,14 +8,13 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/mattdonnelly/Swifter", from: "2.5.0"),
-        .package(url: "https://github.com/mxcl/PromiseKit", from: "6.13.3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GitGrassIndicator",
-            dependencies: ["Swifter", "PromiseKit"]),
+            dependencies: ["Swifter"]),
         .testTarget(
             name: "GitGrassIndicatorTests",
             dependencies: ["GitGrassIndicator"]),
