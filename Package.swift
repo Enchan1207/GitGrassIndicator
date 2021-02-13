eef7ca8dda870926013442dc628ae244749d588a
Enchan <enchant0130@gmail.com>
2021-02-13 13:16:52 +0900

    [Modify] Package.swift  - Add dependency "ArgumentParser"

--- Package.swift
+++ Package.swift
@@ -8,13 +8,17 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/mattdonnelly/Swifter", from: "2.5.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GitGrassIndicator",
-            dependencies: ["Swifter"]),
+            dependencies: [
+                "Swifter",
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]),
         .testTarget(
             name: "GitGrassIndicatorTests",
             dependencies: ["GitGrassIndicator"]),
